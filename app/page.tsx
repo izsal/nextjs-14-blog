a168486e355a77788fb263c2b23a6a8705d1954e
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2024-09-11 13:51:30 +0700

    feat: 🎸 add profile

--- app/page.tsx
+++ app/page.tsx
@@ -26,28 +26,47 @@ export default async function Home() {
   // console.log(data);
 
   return (
-    <div className="grid grid-col-1 md:grid-cols-2 mt-5 gap-5">
-      {data.map((post, idx) => (
-        <Card key={idx}>
-          <Image
-            src={urlFor(post.titleImage).url()}
-            alt="image_blog"
-            width={500}
-            height={500}
-            className="rounded-t-lg h-[200px] object-cover"
-          />
+    <>
+      <div className="flex gap-3 sm:gap-5 justify-left items-center">
+        <Image
+          src={"/vercel.svg"}
+          alt="vercel logo"
+          width={64}
+          height={64}
+          className="p-4 shadow-sm w-16 h-16 rounded-full border-2 border-sky-500"
+        />
+        <div className="sm:px-0 px-2">
+          <h1 className="text-2xl sm:text-3xl text-slate-800 dark:text-slate-300">
+            Izsal Qurlinas Afandi
+          </h1>
+          <h2 className="text-xl sm:text-2xl text-slate-700 dark:text-slate-300 font-light">
+            Web Developer
+          </h2>
+        </div>
+      </div>
+      <div className="grid grid-col-1 md:grid-cols-2 mt-5 gap-5">
+        {data.map((post, idx) => (
+          <Card key={idx}>
+            <Image
+              src={urlFor(post.titleImage).url()}
+              alt="image_blog"
+              width={500}
+              height={500}
+              className="rounded-t-lg h-[200px] object-cover"
+            />
 
-          <CardContent className="mt-5">
-            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
-            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
-              {post.smallDescription}
-            </p>
-            <Button asChild className="w-full mt-7">
-              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
-            </Button>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+            <CardContent className="mt-5">
+              <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+                {post.smallDescription}
+              </p>
+              <Button asChild className="w-full mt-7">
+                <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
